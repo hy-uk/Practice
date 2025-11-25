bea2dbad2b208998361cf804b3d10e516314cad6
유형욱 <hy_uk000@naver.com>
2025-11-25 13:59:38 +0900

    디버깅 테스트

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-def solution(num1, num2):
-    if num1 == num2:
-        answer = 1
+def std_weight(height, gender):
+    if gender == "남자":
+        return height * height * 22
     else:
-        asnswer = -1
-    return answer
+        return height * height * 21
+    
+height = 175
+gender = "남성"
+weight=std_weight(height / 100,gender)
+weight = round(weight,2)
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height,gender,weight))
+
